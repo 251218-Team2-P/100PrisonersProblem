bb3ae164da2eb73000f4befa42cec5f00ae9ce1f
JackyKIMSungSu <sskim7415@gmail.com>
2025-12-18 18:09:19 +0900

    feat: Init make file

---
+++ play_optimal.py
@@ -0,0 +1,7 @@
+def play_optimal(n):
+    pass
+
+if __name__ == "__main__":
+    n = 100_000
+    print(" Simulation count:", n)
+    print(f" Optimal play wins: {play_optimal(n):4.1f}% of simulations")
---
+++ play_random.py
@@ -0,0 +1,7 @@
+def play_random(n):
+    pass
+
+if __name__ == "__main__":
+    n = 100_000
+    print(" Simulation count:", n)
+    print(f" Random play wins: {play_random(n):4.1f}% of simulations")
